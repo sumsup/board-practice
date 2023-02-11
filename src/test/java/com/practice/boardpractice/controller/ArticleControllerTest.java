69e546c46af065e91ffdf8d15c8b5fc28400cecd
sums-up <starirof@naver.com>
2023-02-12 02:28:36 +0900

    테스트 코드에 컨트롤러 추가

--- src/test/java/com/practice/boardpractice/controller/ArticleControllerTest.java
+++ src/test/java/com/practice/boardpractice/controller/ArticleControllerTest.java
@@ -6,14 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 게시글")
-@WebMvcTest
+@WebMvcTest(ArticleController.class) // 입력된 컨트롤러만 로딩해서 테스트 수행.
 class ArticleControllerTest {
 
     private final MockMvc mvc;
