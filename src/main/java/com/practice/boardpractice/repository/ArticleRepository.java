ead206888b94ee25820cfcc3b85109f5006caf61
sums-up <starirof@naver.com>
2023-04-04 05:52:27 +0900

    스프링 시큐리티 인증 MVC 구현

--- src/main/java/com/practice/boardpractice/repository/ArticleRepository.java
+++ src/main/java/com/practice/boardpractice/repository/ArticleRepository.java
@@ -26,6 +26,8 @@ public interface ArticleRepository extends
     Page<Article> findByUserAccount_NicknameContaining(String nickname, Pageable pageable);
     Page<Article> findByHashtag(String hashtag, Pageable pageable);
 
+    void deleteByIdAndUserAccount_UserId(Long articleId, String userid);
+
     @Override
     default void customize(QuerydslBindings bindings, QArticle root) {
         // 검색 기능에서 제외하기 위함.
